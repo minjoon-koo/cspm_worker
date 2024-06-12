e8fc1492bc3671baa25dbfbcc14b9550509b2289
minjoon-koo <cubus.secu@gmail.com>
2024-06-12 18:06:05 +0900

    cspmWorker: github action to use node.js 20

--- install.sh
+++ install.sh
@@ -15,4 +15,11 @@
 #powerpipe mod install github.com/turbot/steampipe-mod-azure-insights
 #powerpipe mod install github.com/turbot/steampipe-mod-azure-compliance
 
+cd /app/worker
+go mod tidy
+go get github.com/gofiber/fiber/v2
+go get github.com/gofiber/fiber/v2/middleware/cors
+go get github.com/gofiber/fiber/v2/middleware/logger
+go get github.com/joho/godotenv
+
 go run /app/worker/main.go
