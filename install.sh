c2fa4545638efd01f2b3dc9af343dfcc951601fc
minjoon-koo <cubus.secu@gmail.com>
2024-06-12 17:41:05 +0900

    cspmWorker: github action to use node.js 20

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #install steampipe
-git clone https://github.com/turbot/steampipe
-cd steampipe/
-sudo make
+#git clone https://github.com/turbot/steampipe
+#cd steampipe/
+#sudo make
 
 #install azure plugin
-steampipe plugin install azure
-steampipe plugin install azuread
+#steampipe plugin install azure
+#steampipe plugin install azuread
 
 #install powerpipe, azure_mod
-sudo /bin/sh -c "$(curl -fsSL https://powerpipe.io/install/powerpipe.sh)"
-mkdir dashboards
-cd dashboards
-powerpipe mod init
-powerpipe mod install github.com/turbot/steampipe-mod-azure-insights
-powerpipe mod install github.com/turbot/steampipe-mod-azure-compliance
+#sudo /bin/sh -c "$(curl -fsSL https://powerpipe.io/install/powerpipe.sh)"
+#mkdir dashboards
+#cd dashboards
+#powerpipe mod init
+#powerpipe mod install github.com/turbot/steampipe-mod-azure-insights
+#powerpipe mod install github.com/turbot/steampipe-mod-azure-compliance
 
 go run /app/worker/main.go
