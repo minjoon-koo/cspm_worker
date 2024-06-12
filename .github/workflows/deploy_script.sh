4f53f10b2e0aac88f2a138e7bff88f22c8902d4a
minjoon-koo <cubus.secu@gmail.com>
2024-06-12 14:21:03 +0900

    cspmWorker: github action deploy

---
+++ .github/workflows/deploy_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd /app
+git pull origin main
+
+cd /app/worker
+
+#install steampipe
+git clone https://github.com/turbot/steampipe
+cd steampipe/
+sudo make
+
+#install azure plugin
+steampipe plugin install azure
+steampipe plugin install azuread
+
+#install powerpipe, azure_mod
+sudo /bin/sh -c "$(curl -fsSL https://powerpipe.io/install/powerpipe.sh)"
+mkdir dashboards
+cd dashboards
+powerpipe mod init
+powerpipe mod install github.com/turbot/steampipe-mod-azure-insights
+powerpipe mod install github.com/turbot/steampipe-mod-azure-compliance
+
+cd /app/worker
+go run main.go
