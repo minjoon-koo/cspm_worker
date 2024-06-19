13858335483a2f0c1936ac955b9940d4c303be72
minjoon-koo <cubus.secu@gmail.com>
2024-06-19 11:37:17 +0900

    cspmWorker: deploy test

--- .github/workflows/deploy_script.sh
+++ .github/workflows/deploy_script.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
+
 cd /app
 git pull origin main
 
 cd /app/worker
 
-#install steampipe
-git clone https://github.com/turbot/steampipe
+# Install steampipe
+if [ ! -d "steampipe" ]; then
+  git clone https://github.com/turbot/steampipe
+fi
 cd steampipe/
 sudo make
 
-#install azure plugin
+# Install azure plugin
 steampipe plugin install azure
 steampipe plugin install azuread
 
-#install powerpipe, azure_mod
+# Install powerpipe and azure_mod
 sudo /bin/sh -c "$(curl -fsSL https://powerpipe.io/install/powerpipe.sh)"
-mkdir dashboards
+mkdir -p dashboards
 cd dashboards
 powerpipe mod init
 powerpipe mod install github.com/turbot/steampipe-mod-azure-insights
 powerpipe mod install github.com/turbot/steampipe-mod-azure-compliance
 
 cd /app/worker
-go build /app/worker/main.go
-nohup ./main &
+go build -o main /app/worker/main.go
+nohup ./main > /dev/null 2>&1 &
+disown
 
 echo "Deployment script executed successfully"
