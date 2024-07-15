11bd8023e7b2a02bff24ea0ea9d871068776ae39
minjoon-koo <cubus.secu@gmail.com>
2024-07-15 12:09:44 +0900

    cspmWorker: deploy.yml Auto IP allow

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,3 @@ git add .
 git commit -m "cspmWorker: host&port"
 git push origin main
 
-
-
-az ad sp create-for-rbac --name "github-actions-deploy" --role contributor --scopes /subscriptions/<subscription-id>/resourceGroups/<resource-group> --sdk-auth
-az ad sp create-for-rbac --name "github-actions-deploy" --role contributor --scopes /subscriptions/6c47a347-6277-47ea-bc05-aa9f2afc5ccd/resourceGroups/rg-soldout --sdk-auth
